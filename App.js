8796c7d17a8b61e994076d54712907b9e4809808
kisyular <rellika@kisyula.com>
2022-12-12 13:05:39 +0300

    SETUP BASIC NAVIGATION AND ADDED IMAGES ASSETS

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
-import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
 import { TailwindProvider } from 'tailwindcss-react-native'
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from './src/screens/HomeScreen'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
 	return (
 		<TailwindProvider>
 			<SafeAreaProvider>
-				<SafeAreaView>
-					<Text className='text-xl text-red-400'>Test!</Text>
-					<StatusBar style='auto' />
-				</SafeAreaView>
+				<NavigationContainer>
+					<Stack.Navigator>
+						<Stack.Screen name='Home' component={HomeScreen} />
+					</Stack.Navigator>
+				</NavigationContainer>
 			</SafeAreaProvider>
 		</TailwindProvider>
 	)
