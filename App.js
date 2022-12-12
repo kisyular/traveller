4387e2798d726e290da309b5fb980015f45f6a06
kisyular <rellika@kisyula.com>
2022-12-12 12:16:16 +0300

    INITIALIZED EXPO APP AND ADDED TAILWIND CSS

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+import { TailwindProvider } from 'tailwindcss-react-native'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<TailwindProvider>
+			<SafeAreaProvider>
+				<SafeAreaView>
+					<Text className='text-xl text-red-400'>Test!</Text>
+					<StatusBar style='auto' />
+				</SafeAreaView>
+			</SafeAreaProvider>
+		</TailwindProvider>
+	)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+	content: [],
+	content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
